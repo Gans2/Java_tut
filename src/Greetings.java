55f721ffeccc706b449be912cfd274685e060e4d
ccl1938 <ccl1938@naver.com>
2021-06-06 20:59:37 +0900

    Step two - String(문자열) 변수 만들기

---
+++ src/Greetings.java
@@ -0,0 +1,11 @@
+public class Greetings {
+    public static void main(String[] args){
+        String name;
+        //문자열(String) 변수 만들기
+        name= "Gans";
+
+        System.out.println("안녕하세요 반갑습니다");
+        System.out.println(name);
+        System.out.println("입니다. 잘 부탁드립니다");
+    }
+}
