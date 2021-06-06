08694b76c40cca2b37aa45c42305e6018f9f1d7f
ccl1938 <ccl1938@naver.com>
2021-06-06 21:15:34 +0900

    Step three - int,double(정수형, 실수형) 변수 만들기

---
+++ src/Exam.java
@@ -0,0 +1,20 @@
+public class Exam {
+    public static void main(String[] args){
+        //정수형 변수 생성
+        int MS;
+        int FS;
+
+        //실수형 변수 생성
+        double RS;
+
+
+        //변수 속 값 대입
+        MS = 0;
+        FS = 0;
+        RS = 42.3;
+
+        System.out.println(MS+" 점");
+        System.out.println(FS+" 점");
+        System.out.println(RS+" 점");
+    }
+}
