62582be71b87b1ddaf68b869bf7fa13691037704
ccl1938 <ccl1938@naver.com>
2021-06-07 11:00:49 +0900

    Step four - boolean(논리형) 변수 만들기

--- src/Nonri.java
+++ src/Nonri.java
@@ -6,7 +6,7 @@ public class Nonri {
         //논리 값 대입
         really = true;
 
-        System.out.println("당신읜 진정 사람이 맞습니까?");
+        System.out.println("당신은 진정 사람이 맞습니까?");
         System.out.println(really);
     }
 }
