32cce1b3a682d0cb1be36f394eed7fb2076df360
ccl1938 <ccl1938@naver.com>
2021-06-07 11:08:54 +0900

    Step five - 변수값 변경

---
+++ src/Byeonsuchange.java
@@ -0,0 +1,17 @@
+public class Byeonsuchange {
+    public static void main(String[] args){
+        //변수 초기값 생성
+        String MS = "첫 번째 메세지입니다.";
+
+        //초기값 출력
+
+        System.out.println(MS);
+
+        //변수값 변경
+
+        MS = "두 번째 메세지입니다.";
+
+        //변경된 변수값 출력
+        System.out.println(MS);
+    }
+}
