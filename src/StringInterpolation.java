649fc06c7f1f703480c6b82589c009069e0967ba
ccl1938 <ccl1938@naver.com>
2021-06-08 10:34:38 +0900

    Step seven - printf()기능 사용

---
+++ src/StringInterpolation.java
@@ -0,0 +1,12 @@
+public class StringInterpolation {
+    public static void main(String[] args){
+        //변수 생성
+        int age = 25;
+
+        //A - 결과 출력
+        System.out.println("본인의 나이는 " + age + "세 입니다");
+
+        //B - 결과 출력
+        System.out.printf("본인의 나이는 %d세 입니다", age);
+    }
+}
