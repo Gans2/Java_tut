95cc6823bf5bfbe599fc594a98a18997be43c69b
ccl1938 <ccl1938@naver.com>
2021-06-08 10:33:52 +0900

    Step six - double(실수형) 변수 계산

---
+++ src/Ipryeokgap.java
@@ -0,0 +1,8 @@
+public class Ipryeokgap {
+    public static void main(String[] args){
+        double f = 77.0;
+        double c = (f - 32.0)/1.8;
+
+        System.out.println("화씨 "+ f +"도는 섭씨로 "+ c +"도 입니다!" );
+    }
+}
