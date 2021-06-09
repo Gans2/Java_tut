e2ff4e73bf48c3a6d1bcb1aa092dc63740c352f7
ccl1938 <ccl1938@naver.com>
2021-06-09 10:36:47 +0900

    Step eight - printf 메소드 출력 형식

---
+++ src/Printftype.java
@@ -0,0 +1,18 @@
+public class Printftype {
+    public static void main(String[] args){
+
+        int i = 3;
+        double d = 3.0;
+        String s = "문자열";
+
+
+        /*printf() 메소드의 출력형식
+        %d = 정수
+        %f = 실수
+        %s = 문자열
+        */
+        System.out.printf("%d",i);
+        System.out.printf("%f",d);
+        System.out.printf("%s",s);
+    }
+}
